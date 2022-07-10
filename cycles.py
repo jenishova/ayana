53dd99d0f2890e2787c454a421573a7eac26db9b
jenishova <108532841+jenishova@users.noreply.github.com>
2022-07-10 01:00:17 -0700

    Add files via upload

---
+++ cycles.py
@@ -0,0 +1,52 @@
+≈y
+#1
+
+languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+for language in languages:
+    print(language)
+
+
+#2
+
+
+languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+for i in languages:
+   i  = php
+
+    
+ 
+#3
+
+a= 7**5
+print(a)
+
+
+#4
+
+
+languages = ['1go', '2java', '3php', '4python', '5javascript', '6ruby']
+for language in languages:
+    print(language)
+
+
+#5
+print(1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1)
+
+#6
+names = ('Максат','Лязат','Данияр','Айбек','Атай','Салават','Адинай','Жоомарт','Алымбек','Эрмек','Дастан','Бекмамат','Аслан',)
+for i in range (1, 30):
+    if i % 2  == 0:
+        print(str(i + ' =even'))
+else:
+    print(str(i + ' =odd)
+
+#7
+
+names = ('Максат','Лязат','Данияр','Айбек','Атай','Салават','Адинай','Жоомарт','Алымбек','Эрмек','Дастан','Бекмамат','Аслан',)
+t=names.split()
+print(t)
+
+
+#8
+
+ 
