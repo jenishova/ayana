f50bd2f25c70d3ed905dec498d1181cfa40ffeee
jenishova <108532841+jenishova@users.noreply.github.com>
2022-07-14 03:01:48 -0700

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,23 @@
+'''
+#1
+print(input("Я функция из my_module.py"))
+
+#2
+names = ["Aibek", "Joomart", "Adinai", "Ermek", "Atai", "Aslan", "Lyazat", "Salavat", "Daniyar", "Bolotbek", "Alymbek", "Dastan", "Maksat"]
+random.shuffle(names)
+print(names)
+random_names = random.choice(names)
+print(random_names)
+'''
+
+#3
+import sys
+print(sys.argv)
+
+
+#4
+
+print('ok')
+
+
+#5
