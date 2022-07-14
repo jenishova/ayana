1fe638f4a81888760ddbaaa00cec10a3fec38dd4
jenishova <108532841+jenishova@users.noreply.github.com>
2022-07-14 01:32:38 -0700

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,48 @@
+
+#1
+
+
+numbers = [10, 20, 30, 40, 50, 60 ,70, 80 ,90]
+try:
+   a = 'tata' / 12
+   print(a[10])
+   print(cooky)
+except(TypeError, IndexError, NameError) as n:
+    print(TypeError, IndexError, NameError)
+
+
+#2
+
+at = 10
+in = 15
+wo = 20
+
+for e in range(-at, at):
+    print(wo / e)
+if at > '5':
+    print("at > 5)
+
+except Exception as a:
+    print('Erroe=e', a)
+
+
+#3
+
+lst = []
+
+for i in range(10):
+    lst.append(i)
+    a = list(reversed(lst))
+    sls_obj = slice('0','8')
+print(a, sls_obj)
+
+
+#4
+
+a = 0
+b = 1
+numbers = []
+while b > a:
+     numbers.append(b)
+     b += 1
+
